81c8d25a6de5815e144e2cda422629edfc76d726
shin-ito0306 <shin.20000306@icloud.com>
2021-06-28 03:50:43 +0000

    [add]create,index

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,28 @@
 class GroupsController < ApplicationController
+  
+  def index
+    @groups = Group.all
+  end
+  
   def new
     @group = Group.new
   end
+  
+  def create
+    @group = Group.new(group_params)
+    @group.owner_id = current_user.id
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
 
   def edit
+  end
+  
+  private
+  def group_params
+    params.require(:group).permit(:name, :introduction, :image)
   end
 end
